9cf6e1061cb51bc05ffb2c539e3602fa16540a70
MinjunSong <minjunevmf2@gmail.com>
2024-12-03 12:10:06 +0900

    major patch
    - 최근 3일간 DB에 추가된 영화 목록을 보여줌
    - ai/urls 경로 오류수정

--- kobis/urls.py
+++ kobis/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import (
     DailyBoxOfficeAPIView, BoxOfficeBannerAPIView, MovieDetailAPIView, SimilarMoviesAPIView,
-    FilterAndSortAPIView, MovieSearchAPIView
+    FilterAndSortAPIView, MovieSearchAPIView, RecentMoviesAPIView
     )
 
 urlpatterns = [
     path('api/boxoffice/daily/', DailyBoxOfficeAPIView.as_view(), name='boxoffice'),
     path('api/boxoffice/banner/', BoxOfficeBannerAPIView.as_view(), name='boxoffice_banner'),
+    path('api/movies/recent/', RecentMoviesAPIView.as_view(), name='recent-movies'),
     path('api/movies/<str:movieCd>/', MovieDetailAPIView.as_view(), name='movie_detail'),
     path('api/movies/<str:movieCd>/similar/', SimilarMoviesAPIView.as_view(), name='similar-movies'),
     path('api/filter-and-sort/movies/', FilterAndSortAPIView.as_view(), name='filter-and-sort'),
